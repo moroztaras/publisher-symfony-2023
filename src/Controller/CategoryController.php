e677540358ae05a44bbe779ea20b8141242bbf76
tarasmoroz <taras.moroz@ekreative.com>
2023-04-06 14:54:06 +0200

    API Swagger doc

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Manager\CategoryManager;
+use App\Model\CategoryListResponse;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +18,14 @@ class CategoryController extends AbstractController
     }
 
     // List all categories
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns book categories",
+     *
+     *     @Model(type=CategoryListResponse::class)
+     * )
+     */
     #[Route(path: '/api/book/categories', methods: ['GET'])]
      public function categories(): Response
      {
