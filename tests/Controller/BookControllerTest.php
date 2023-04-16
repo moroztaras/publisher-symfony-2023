1393a7eaf9c62401926175d2934f2d59e5710c3c
tarasmoroz <taras.moroz@ekreative.com>
2023-04-16 18:10:53 +0200

    Changed BookControllerTest

--- tests/Controller/BookControllerTest.php
+++ tests/Controller/BookControllerTest.php
@@ -2,27 +2,74 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\Book;
+use App\Entity\Category;
+use App\Tests\AbstractControllerTest;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Request;
 
-class BookControllerTest extends WebTestCase
+class BookControllerTest extends AbstractControllerTest
 {
     public function testBooksByCategory()
     {
-        $expectedFile = __DIR__.'/BookControllerTest/testBooksByCategory.json';
+        $categoryId = $this->createCategory();
 
-        // Create client
-        $client = static::createClient();
         // Create request
-        $client->request(Request::METHOD_GET, '/api/category/1/books');
+        $this->client->request(Request::METHOD_GET, '/api/category/'.$categoryId.'/books');
 
         // Get response content
-        $responseContent = $client->getResponse()->getContent();
+        $responseContent = json_decode($this->client->getResponse()->getContent(), true);
 
         // Checking the success of the request.
         $this->assertResponseIsSuccessful();
 
         // Check matching the expected value from the actual value
-        $this->assertJsonStringEqualsJsonFile($expectedFile, $responseContent);
+        $this->assertJsonDocumentMatchesSchema($responseContent, [
+            'type' => 'object',
+            'required' => ['items'],
+            'properties' => [
+                'items' => [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'object',
+                        'required' => ['id', 'title', 'slug', /* 'image', */ 'authors', 'meap', 'publicationAt'],
+                        'properties' => [
+                            'title' => ['type' => 'string'],
+                            'slug' => ['type' => 'string'],
+                            'id' => ['type' => 'integer'],
+                            'publicationAt' => ['type' => 'integer'],
+//                            'image' => ['type' => 'string'],
+                            'meap' => ['type' => 'boolean'],
+                            'authors' => [
+                                'type' => 'array',
+                                'items' => ['type' => 'string'],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    private function createCategory(): int
+    {
+        // Create category
+        $category = (new Category())->setTitle('Devices')->setSlug('devices');
+        // Persist category
+        $this->em->persist($category);
+
+        $this->em->persist((new Book())
+            ->setTitle('Test book')
+            ->setImage('http://localhost.png')
+            ->setMeap(true)
+            ->setPublicationAt(new \DateTime())
+            ->setAuthors(['Tester'])
+            ->setCategories(new ArrayCollection([$category]))
+            ->setSlug('test-book')
+        );
+
+        $this->em->flush();
+
+        return $category->getId();
     }
 }
