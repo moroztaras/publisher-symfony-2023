03274b35d2d2ca9267a28b15f7b5d4d092818a52
tarasmoroz <taras.moroz@ekreative.com>
2023-04-21 14:57:07 +0200

    Added validator

---
+++ src/Listener/ValidationExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listener;
+
+use App\Exception\ValidationException;
+use App\Model\ErrorResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ValidationExceptionListener
+{
+    public function __construct(
+        private SerializerInterface $serializer
+    ) {
+    }
+
+    public function __invoke(ExceptionEvent $event): void
+    {
+        // Get an exception
+        $throwable = $event->getThrowable();
+        if (!($throwable instanceof ValidationException)) {
+            return;
+        }
+
+        // Form an answer
+        $data = $this->serializer->serialize(
+            new ErrorResponse($throwable->getMessage(), ['violations' => $throwable->getViolations()]),
+            JsonEncoder::FORMAT,
+        );
+
+        // Response
+        $event->setResponse(new JsonResponse($data, Response::HTTP_BAD_REQUEST, [], true));
+    }
+}
