483b807fcb971b975c473ba79c451fb34f469abd
tarasmoroz <taras.moroz@ekreative.com>
2023-04-27 12:56:37 +0200

    Added testInvoke

--- tests/Listener/ValidationExceptionListenerTest.php
+++ tests/Listener/ValidationExceptionListenerTest.php
@@ -2,9 +2,16 @@
 
 namespace App\Tests\Listener;
 
+use App\Exception\ValidationException;
 use App\Listener\ValidationExceptionListener;
+use App\Model\ErrorResponse;
+use App\Model\ErrorValidationDetails;
 use App\Tests\AbstractTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
 
 class ValidationExceptionListenerTest extends AbstractTestCase
 {
@@ -29,5 +36,49 @@ class ValidationExceptionListenerTest extends AbstractTestCase
         (new ValidationExceptionListener($this->serializer))($event);
     }
 
+    public function testInvoke(): void
+    {
+        // At the output, we want to get an object
+        $serialized = json_encode([
+            'message' => 'validation failed',
+            'details' => [
+                'violations' => [
+                    ['field' => 'name', 'message' => 'error'],
+                ],
+            ],
+        ]);
 
+        // Create event
+        $event = $this->createExceptionEvent(new ValidationException(new ConstraintViolationList([
+            new ConstraintViolation('error', null, [], null, 'name', null),
+        ])));
+
+        $this->serializer->expects($this->once())
+            ->method('serialize')
+            ->with(
+                $this->callback(function (ErrorResponse $response) {
+                    /** @var ErrorValidationDetails|object $details */
+                    $details = $response->getDetails();
+
+                    if (!($details instanceof ErrorValidationDetails)) {
+                        return false;
+                    }
+
+                    $violations = $details->getViolations();
+                    if (1 !== count($violations) || 'validation failed' !== $response->getMessage()) {
+                        return false;
+                    }
+
+                    return 'name' === $violations[0]->getField() && 'error' === $violations[0]->getMessage();
+                }),
+                JsonEncoder::FORMAT
+            )
+            ->willReturn($serialized);
+
+        // Run ValidationExceptionListener
+        (new ValidationExceptionListener($this->serializer))($event);
+
+        // Check response
+        $this->assertResponse(Response::HTTP_BAD_REQUEST, $serialized, $event->getResponse());
+    }
 }
