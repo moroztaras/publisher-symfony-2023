51c71312fee67db2f4a51e3d36518c5da9ca0042
tarasmoroz <taras.moroz@ekreative.com>
2023-05-03 14:58:47 +0200

    Added testGetReviewPageByBookId

--- tests/Manager/ReviewManagerTest.php
+++ tests/Manager/ReviewManagerTest.php
@@ -2,8 +2,10 @@
 
 namespace App\Tests\Manager;
 
+use App\Entity\Review;
 use App\Manager\RatingManager;
 use App\Manager\ReviewManager;
+use App\Model\Review as ReviewModel;
 use App\Model\ReviewPage;
 use App\Repository\ReviewRepository;
 use App\Tests\AbstractTestCase;
@@ -56,11 +58,48 @@ class ReviewManagerTest extends AbstractTestCase
 
         // Create ReviewManager
         $reviewManager = new ReviewManager($this->reviewRepository, $this->ratingManager);
-        // Waiting expected value
+        // Expected value
         $expected = (new ReviewPage())->setTotal(0)->setRating(0)->setPage($page)->setPages(0)
             ->setPerPage(self::PER_PAGE)->setItems([]);
 
         // Check matching the expected value from the actual value
         $this->assertEquals($expected, $reviewManager->getReviewPageByBookId(self::BOOK_ID, $page));
+    }
+
+    public function testGetReviewPageByBookId(): void
+    {
+        // Set the behavior for the method - calcReviewRatingForBook
+        $this->ratingManager->expects($this->once())
+            ->method('calcReviewRatingForBook')
+            ->with(self::BOOK_ID, 1)
+            ->willReturn(4.0);
+
+        $review = (new Review())->setAuthor('tester')->setContent('test content')
+            ->setCreatedAt(new \DateTimeImmutable('2020-10-10'))->setRating(4);
+
+        // Set id for Review
+        $this->setEntityId($review, 1);
+
+        // Set the behavior for the method - getPageByBookId
+        $this->reviewRepository->expects($this->once())
+            ->method('getPageByBookId')
+            ->with(self::BOOK_ID, 0, self::PER_PAGE)
+            ->willReturn(new \ArrayIterator([$review]));
+
+        // Create ReviewManager
+        $reviewManager = new ReviewManager($this->reviewRepository, $this->ratingManager);
+        // Expected value
+        $expected = (new ReviewPage())->setTotal(1)->setRating(4)->setPage(1)->setPages(1)
+            ->setPerPage(self::PER_PAGE)->setItems([
+                (new ReviewModel())
+                    ->setId(1)
+                    ->setRating(4)
+                    ->setCreatedAt(1602288000)
+                    ->setContent('test content')
+                    ->setAuthor('tester'),
+            ]);
+
+        // Check matching the expected value from the actual value
+        $this->assertEquals($expected, $reviewManager->getReviewPageByBookId(self::BOOK_ID, 1));
     }
 }
