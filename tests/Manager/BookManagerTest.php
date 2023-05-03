50c0083aa2cf579cc32521fb1f06d12caf7173a9
tarasmoroz <taras.moroz@ekreative.com>
2023-05-03 13:24:32 +0200

    Changed tests in BookManagerTest

--- tests/Manager/BookManagerTest.php
+++ tests/Manager/BookManagerTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Manager;
 use App\Entity\Book;
 use App\Exception\CategoryNotFoundException;
 use App\Manager\BookManager;
+use App\Manager\RatingManager;
 use App\Model\BookListItem;
 use App\Model\BookListResponse;
 use App\Repository\BookRepository;
@@ -16,17 +17,28 @@ use Doctrine\Common\Collections\ArrayCollection;
 class BookManagerTest extends AbstractTestCase
 {
     // Unit tests
+    private ReviewRepository $reviewRepository;
+
+    private BookRepository $bookRepository;
+
+    private CategoryRepository $categoryRepository;
+
+    private RatingManager $ratingManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // Empty mock for repositories and manager
+        $this->reviewRepository = $this->createMock(ReviewRepository::class);
+        $this->bookRepository = $this->createMock(BookRepository::class);
+        $this->categoryRepository = $this->createMock(CategoryRepository::class);
+        $this->ratingManager = $this->createMock(RatingManager::class);
+    }
+
     public function testGetBooksByCategoryNotFound(): void
     {
-        // Empty mock for ReviewRepository
-        $reviewRepository = $this->createMock(ReviewRepository::class);
-        // Empty mock for BookRepository
-        $bookRepository = $this->createMock(BookRepository::class);
-        // Empty mock for CategoryRepository
-        $categoryRepository = $this->createMock(CategoryRepository::class);
-
         // Set behaved method - existsById in CategoryRepository
-        $categoryRepository->expects($this->once())
+        $this->categoryRepository->expects($this->once())
             ->method('existsById')
             ->with(130)
             ->willReturn(false)
@@ -35,37 +47,42 @@ class BookManagerTest extends AbstractTestCase
         $this->expectException(CategoryNotFoundException::class);
 
         // We call method - getBooksByCategory
-        (new BookManager($bookRepository, $categoryRepository, $reviewRepository))->getBooksByCategory(130);
+        $this->createBookManager()->getBooksByCategory(130);
     }
 
     public function testGetBooksByCategory(): void
     {
-        // Empty mock for ReviewRepository
-        $reviewRepository = $this->createMock(ReviewRepository::class);
-        // Empty mock for BookRepository
-        $bookRepository = $this->createMock(BookRepository::class);
-        // Set behaved method - findBooksByCategoryId in BookRepository
-        $bookRepository->expects($this->once())
+        // Set behaved method - findPublishedBooksByCategoryId in BookRepository
+        $this->bookRepository->expects($this->once())
             ->method('findPublishedBooksByCategoryId')
             ->with(130)
             ->willReturn([$this->createBookEntity()]);
 
-        // Empty mock for CategoryRepository
-        $categoryRepository = $this->createMock(CategoryRepository::class);
         // Set behaved method - existsById in CategoryRepository
-        $categoryRepository->expects($this->once())
+        $this->categoryRepository->expects($this->once())
             ->method('existsById')
             ->with(130)
             ->willReturn(true);
 
-        $bookManager = new BookManager($bookRepository, $categoryRepository, $reviewRepository);
         // Expected value
         $expected = new BookListResponse([$this->createBookItemModel()]);
 
         // Check matching the expected value from the actual value
-        $this->assertEquals($expected, $bookManager->getBooksByCategory(130));
+        $this->assertEquals($expected, $this->createBookManager()->getBooksByCategory(130));
     }
 
+    // Create BookManager
+    private function createBookManager(): BookManager
+    {
+        return new BookManager(
+            $this->bookRepository,
+            $this->categoryRepository,
+            $this->reviewRepository,
+            $this->ratingManager
+        );
+    }
+
+    // Create entity book
     private function createBookEntity(): Book
     {
         $book = (new Book())
@@ -73,7 +90,7 @@ class BookManagerTest extends AbstractTestCase
             ->setSlug('test-book')
             ->setMeap(false)
             ->setIsbn('123321')
-            ->setDescription('Test description')
+            ->setDescription('test description')
             ->setAuthors(['Tester'])
             ->setImage('http://localhost/test.png')
             ->setCategories(new ArrayCollection())
@@ -84,6 +101,7 @@ class BookManagerTest extends AbstractTestCase
         return $book;
     }
 
+    // Create Book Item Model
     private function createBookItemModel(): BookListItem
     {
         return (new BookListItem())
@@ -92,7 +110,7 @@ class BookManagerTest extends AbstractTestCase
             ->setSlug('test-book')
             ->setMeap(false)
             ->setAuthors(['Tester'])
-//            ->setImage('http://localhost/test.png')
+            ->setImage('http://localhost/test.png')
             ->setPublicationAt(1602288000);
     }
 }
